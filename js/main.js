60f7930091c4d7e6f26f63a5f84d90ed9290ec3e
michaelpaulsen <michael.j.paulsen@gmail.com>
2024-11-12 19:12:02 -0800

    misc formatting updates

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 let prev = "unknown";
-let t, render=true; 
-let playbtnDefault = $("#record_pause_btn").val(); 
+let t, render=true;
+let playbtnDefault = $("#record_pause_btn").val();
 let recordingState = "Inactive";
-let writingState = "Inactive"; 
-const PPBTN = $("#record_pause_btn"); 
-let framerate =30, ff =0, ss =0,mm =0,hh =0, interval, inttime = 1000/framerate, gfc =0; 
-let recSymToggles = 0, paused =false; 
-let colorRecBTN = false, colorPauseBTN =true; 
+let writingState = "Inactive";
+const PPBTN = $("#record_pause_btn");
+let framerate =30, ff =0, ss =0,mm =0,hh =0, interval, inttime = 1000/framerate, gfc =0;
+let recSymToggles = 0, paused =false;
+let colorRecBTN = false, colorPauseBTN =true;
 
 $("#btn_add_marker").on("click", () => {
 	$.get("/addMarker", (d) => {
@@ -15,8 +15,8 @@ $("#btn_add_marker").on("click", () => {
 });
 
 $("#change_scene").on("click", () => {
-	let target = $("#to_scene").val(); 
-	let reason =   $("#reason").val(); 
+	let target = $("#to_scene").val();
+	let reason =   $("#reason").val();
 
 	$.ajax({
 		url: "/changeSceneCollection",
@@ -25,20 +25,20 @@ $("#change_scene").on("click", () => {
 			reason
 		},
 		success: (d) => {
-			prev = target; 
 			addMarker(`${reason} (${prev} to ${target})`, d);
+			prev = target;
 
 		},
 		method: "GET",
 	});
-	
+
 });
 
-$("#wl_win").on("click", (e)=>{ 
+$("#wl_win").on("click", (e)=>{
 	$.ajax({
 		url: "/mtg_win",
 		success: (d) => {
-			printStats(d); 
+			printStats(d);
 			return;
 		},
 		method: "GET",
@@ -46,30 +46,30 @@ $("#wl_win").on("click", (e)=>{
 
 });
 
-$("#wl_loss").on("click", (e)=>{ 
+$("#wl_loss").on("click", (e)=>{
 	$.ajax({
 		url: "/mtg_loss",
 		success: (d) => {
-			printStats(d); 
+			printStats(d);
 			return;
 		},
 		method: "GET",
-	}); 
+	});
 
 })
 
-$("#wl_reset").on("click", (e)=>{ 
+$("#wl_reset").on("click", (e)=>{
 	$.ajax({
 		url: "/mtg_reset",
 		success: (d) => {
-			printStats(d); 
+			printStats(d);
 			return;
 		},
 		method: "GET",
-	}); 
-}); 
+	});
+});
 
-$("#refesh_scene_colection").on("click", (e)=> { 
+$("#refesh_scene_colection").on("click", (e)=> {
 	UpdateSceneCollection();
 });
 
