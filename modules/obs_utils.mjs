d5dfe65738c160349786ab757cafd0fd7938d48b
michaelpaulsen <michael.j.paulsen@gmail.com>
2024-11-29 12:06:51 -0800

    jslint of obs_utils.mjs file

--- modules/obs_utils.mjs
+++ modules/obs_utils.mjs
@@ -6,15 +6,15 @@ async function ChangeSceneCollection( res, obs, targetSeneCollection){
 
 			return true;
 		}
-		catch(e){ 
-			let resp = `unable to set scene collection ${e}`; 
+		catch(e){
+			let resp = `unable to set scene collection ${e}`;
 			console.log(resp)
 			json_error(res, resp);
-			return false; 
+			return false;
 		}
-} 
+}
 
-async function centertoScreen(res, obs, scene, sceneItem){ 
+async function centertoScreen(res, obs, scene, sceneItem){
 	let transform = sceneItem["sceneItemTransform"];
 	let { baseWidth, baseHeight } =
 		await obs.call("GetVideoSettings");
@@ -26,57 +26,57 @@ async function centertoScreen(res, obs, scene, sceneItem){
 	transform.boundsWidth = transform["sourceWidth"];
 	transform["positionX"] = xpading / 2;
 	transform["positionY"] = ypading / 2;
-	console.log(transform); 
-	try{ 
+	console.log(transform);
+	try{
 
 		await obs.call("SetSceneItemTransform", {
 			sceneName: scene,
 			sceneItemId: sceneItem.sceneItemId,
 			sceneItemTransform: transform,
 		});
-		return true; 
-	} catch(e){ 
-		console.log(e); 
-		return false; 
+		return true;
+	} catch(e){
+		console.log(e);
+		return false;
 	}
 }
-function filterOutPutList(outputs){ 
-    let ret = []; 
+function filterOutPutList(outputs){
+    let ret = [];
     for (const output of outputs) {
         // output.outputKind != 'replay_buffer'
         let kind = output.outputKind;
         if (
             !(
                 kind == "replay_buffer" ||
-                kind == "virtualcam_output" || 
+                kind == "virtualcam_output" ||
 				kind == "audio"
             )
         ) {
-            ret.push(output); 
+            ret.push(output);
         }
     }
-    return ret; 
+    return ret;
 }
-async function changeScene(res, obs, targetscene) { 
-	try{ 
-	
+async function changeScene(res, obs, targetscene) {
+	try{
+
 		await obs.call("SetCurrentProgramScene", {
 			sceneName: targetscene,
 		});
 	} catch (e) {
-		json_error(res, JSON.stringify(e)); 
+		json_error(res, JSON.stringify(e));
 	}
 }
-async function GetOutputList(res, obs ){ 
+async function GetOutputList(res, obs ){
 	return await obs.call("GetOutputList");
 }
-async function getcurrentTransition(res,obs){ 
+async function getcurrentTransition(res,obs){
 	try {
 		return obs.call("GetCurrentSceneTransition");
 	}
-	catch(e){ 
-		console.error(e); 
-		return false; 
+	catch(e){
+		console.error(e);
+		return false;
 	}
 }
 async function GetSceneCollectionList(obs){
